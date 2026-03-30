4599ce47c1d7764a2c5aba27d3a3ce4c582ab452
Gustinbarbie <kittapatoo@gmail.com>
2026-03-30 08:36:31 +0000

    work

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
+    monster *p;
+
+    monster m1("gay",20,67);
+    monster m2("fang"),m4("pang",30);
+    monster x[5]{{"sugoi",5},{"somsri",5,6}};
+    p=new monster("lulu",10,50);
+    delete p;
     return 0;
 }
--- monster.h
+++ monster.h
@@ -13,9 +13,22 @@ private:
     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string="gay", int=10,int=10);
+    ~monster();
 };
 
+monster::~monster(){
+     cout<<"bye bye "<<name <<" hp: "<<hp<<endl;
+}
+
+monster::monster(string n,int h,int p){
+    name=n;
+    h>200? hp=200:hp=h;
+    p>10?potion=10: potion=p;
+
+    cout<<"Monster "<<name <<" guuuu"<<endl;
+
+}
 
 #endif /* monster_h */
